03faf18ee16cce789bbf32c58658a1e671fac333
BolajiOlajide <bolaji.olajide@andela.com>
2018-10-29 02:52:29 +0100

    remove useless files

--- src/index.js
+++ src/index.js
@@ -1,15 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import LoanPage from './pages/Loan.page';
-import * as serviceWorker from './serviceWorker';
 
 // styles
 import 'antd/dist/antd.css';
 import './index.css';
 
-ReactDOM.render(<LoanPage />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(<LoanPage />, document.getElementById('root'));
