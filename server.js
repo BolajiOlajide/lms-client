b92d0a58490cd22db2d5e23a83c34dd2ecacb323
BolajiOlajide <bolaji.olajide@andela.com>
2018-10-29 02:48:18 +0100

    spin up express server for deployment

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+
+const app = express();
+const PORT = 9000;
+
+app.use(express.static(path.join(__dirname, './build')));
+
+app.get('*', (req, res) => res
+  .sendFile('index.html', { root: path.join(__dirname, '/build') }));
+
+
+app.listen(PORT, (err) => {
+  if (err) {
+    console.log('error ==>', err.message); // eslint-disable-line no-console
+    return false;
+  }
+  console.log(`Server running on PORT ${PORT}`); // eslint-disable-line no-console
+});
